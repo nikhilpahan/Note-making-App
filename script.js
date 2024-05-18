0795d8dafa52c5b7478b3d0b06ea4b780f2ec52e
nikhilpahan <nikhilpahan02@gmail.com>
2024-05-18 22:19:33 +0530

    added next line functionality

--- script.js
+++ script.js
@@ -36,3 +36,10 @@ notesContainer.addEventListener("click", function(e){
         })
     }
 })
+
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
